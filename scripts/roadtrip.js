a4ae7824470db854ec8711f463cbe7ebc3f7c82b
kittybellth <kittybell.th@gmail.com>
2017-04-22 01:25:47 -0500

    initialized auth firebase codes and edited html related to auth codes

--- scripts/roadtrip.js
+++ scripts/roadtrip.js
@@ -1,37 +1,138 @@
-/*
+//  firebase initialization code
 var config = {
-    apiKey: "AIzaSyAFvIkXWFbo56ob8pYcDtsoRe0mG4gG3LA",
-    authDomain: "roadtrip-4398d.firebaseapp.com",
-    databaseURL: "https://roadtrip-4398d.firebaseio.com",
-    projectId: "roadtrip-4398d",
-    storageBucket: "roadtrip-4398d.appspot.com",
-    messagingSenderId: "540869499583"
+    apiKey: "AIzaSyBeeNYWaNWO0P9zd16ZEZDrNFRfeCRkIto",
+    authDomain: "fir-intro-aaf02.firebaseapp.com",
+    databaseURL: "https://fir-intro-aaf02.firebaseio.com",
+    projectId: "fir-intro-aaf02",
+    storageBucket: "fir-intro-aaf02.appspot.com",
+    messagingSenderId: "144411755167"
   };
   firebase.initializeApp(config);
 
+    /**
+     * Handles the sign in button press.
+     */
+    function toggleSignIn(event) {
 
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+      event.preventDefault();
+      if (firebase.auth().currentUser) {
+        // [START signout]
+        firebase.auth().signOut();
+        $(".form-control").removeClass("hide");
+        $("#new-user").removeClass("hide");
+        // [END signout]
+      } else {
+        var email = document.getElementById('user-email').value;
+        var password = document.getElementById('user-password').value;
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (password.length < 4) {
+          alert('Please enter a password.');
+          return;
+        }
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authwithemail]
+      }
+      document.getElementById('sign-in').disabled = true;
+    };
 
+    /**
+     * Handles the sign up button press.
+     */
+    function handleSignUp(event) {
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+      event.preventDefault();
+      var email = document.getElementById('user-email').value;
+      var password = document.getElementById('user-password').value;
+      
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      // [START createwithemail]
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+      // [END createwithemail]
+    };
 
+    function initApp() {
+      // Listening for auth state changes.
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
+       
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+            
+          var welcomeText = $("<h4 class'welcome'>");
+          welcomeText.text("Welcome ! " + email);
+          $(".form-user").append(welcomeText);
+          $("#sign-in").text("Sign Out");
+          $(".form-control").addClass("hide");
+          $("#new-user").addClass("hide");
+          $("#user-email").empty();
+          $("#user-password").empty();
+          // [START_EXCLUDE]
 
-  firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}).catch(function(error) {
-  // An error happened.
-});
-*/
+        } else {
+          // User is signed out.
+          // [START_EXCLUDE]
+          $("#sign-in").text("Sign In");
+        }
+        // [START_EXCLUDE silent]
+        document.getElementById('sign-in').disabled = false;
+        // [END_EXCLUDE]
+      });
+      // [END authstatelistener]
+      document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
+      document.getElementById('new-user').addEventListener('click', handleSignUp, false);
+    }
+
+window.onload = function() {
+      initApp();
+  };
+
+// ============================== End Auth function ====================
+// =====================================================================
 
 $("#search-button-submit").on("click", function(event) {
 
