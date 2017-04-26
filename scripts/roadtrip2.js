d37234c489ff51c818735c152018780e373c068b
kittybellth <kittybell.th@gmail.com>
2017-04-26 18:54:18 -0500

    fixed the sign in codes that handle with new design

--- scripts/roadtrip2.js
+++ scripts/roadtrip2.js
@@ -1,5 +1,5 @@
 var dataSet = [];
-
+var flightData = [];
 
 $("#search-button-submit").on("click", function(event) {
 
@@ -31,14 +31,15 @@ $("#search-button-submit").on("click", function(event) {
             var local = results[i].datetime_local;
 
             var localTime = moment(local).format('MMM Do YY, h:mm a');
-            dataSet.push([results[i].title, localTime, results[i].venue.city + ", " + results[i].venue.state, results[i].venue.name, results[i].stats.lowest_price, results[i].url, response.meta.geolocation.display_name]);
+            var localTime2 = moment(local).format('YYYYMMDD');
+            dataSet.push([results[i].title, localTime, results[i].venue.city + ", " + results[i].venue.state, results[i].venue.name, results[i].stats.lowest_price, results[i].url, response.meta.geolocation.city, results[i].venue.city, localTime2]);
 
-        }
+        };
         console.log(dataSet);
 
         var table = $('#myTable').DataTable({
             responsive: true,
-			"searching": false,
+            "searching": false,
             data: dataSet,
             columns: [
                 { title: "Event" },
@@ -58,23 +59,49 @@ $("#search-button-submit").on("click", function(event) {
         $('#myTable tbody').on('click', 'button', function() {
             console.log(table);
             var data = table.row($(this).parents('tr')).data();
-            
+            $("#flightTable").show();
+            var departureDate = data[8];
+            var destination = cities[data[7]];
+            var departure = cities[data[6]];
+            // HIDE TABLE?? or reload Table
+
+
+
+            var queryURL = "http://developer.goibibo.com/api/search/?app_id=7ebce3b6&app_key=dfb5c7018de2ca739ed1cd79e8c6f793&format=json&source=" + departure + "&destination=" + destination + "&dateofdeparture=" + departureDate + "&seatingclass=E&adults=1&children=0&infants=0&counter=100";
+
+            console.log(queryURL);
+            $.ajax({
+                    url: queryURL,
+                    method: "GET",
+                })
+                .done(function(response) {
+                    console.log(response);
+                    var results = response.data.onwardflights;
+                    for (var i = 0; i < results.length; i++) {
+                        var fare = results[i].fare.totalfare * .016;
+                        flightData.push([results[i].deptime, results[i].arrtime, results[i].CabinClass, results[i].airline, fare]);
+
+                    };
+                    console.log(flightData);
+                    var airTable = $('#flightTable').DataTable({
+                        responsive: true,
+                        "searching": false,
+                        data: dataSet,
+                        columns: [
+                            { title: "Depature Time" },
+                            { title: "Arrival Time" },
+                            { title: "Class" },
+                            { title: "Airline" },
+                            { title: "Price" },
+                        ]
+                    });
+                })
         });
 
-
     });
+
+
 });
 
-$('#myTable').DataTable({
-    responsive: true,
-	"searching": false,
-    data: dataSet,
-    columns: [
-        { title: "Event" },
-        { title: "Date" },
-        { title: "City" },
-        { title: "Venue" },
-        { title: "Lowest Price" },
-        { title: "Purchase" },
-    ]
-});
+
+//moment(departureDate).add(1,'days');
